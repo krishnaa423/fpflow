f971b02700310fcc4f8845f43985835d81e9382f
Srikrishnaa Vadivel <krishnaa@Srikrishnaas-Mac-mini.local>
2025-09-30 16:11:37 -0400

    Added pdos and pdos plot

--- src/fpflow/plots/pdos.py
+++ src/fpflow/plots/pdos.py
@@ -1,10 +1,15 @@
+
 #region modules
 from fpflow.plots.plot import PlotBase, PlotType
 import pandas as pd
 from ase.units import Hartree, eV
 import jmespath
 import xml.etree.ElementTree as ET
-import numpy as np 
+import numpy as np
+import glob
+import os
+import re
+import matplotlib.pyplot as plt 
 
 #endregion
 
@@ -12,6 +17,19 @@ import numpy as np
 #endregion
 
 #region functions
+def pdos_label_from_filename(filename):
+    pattern = r'.*?struct_pdos.dat.pdos_atm#(?P<idx>\d+)\((?P<symbol>\w+)\)_wfc#\d+\((?P<orbital>.*)\)'
+
+    match = re.match(pattern, filename)
+    if match:
+        atom_idx = match.group('idx')
+        atom_symbol = match.group('symbol')
+        orbital = match.group('orbital')
+        label = f'{atom_idx}{atom_symbol}_{orbital}'
+        return label
+    else:
+        raise ValueError(f"Filename '{filename}' does not match the expected pattern.")
+
 #endregion
 
 #region classes
@@ -19,69 +37,92 @@ class PdosPlot(PlotBase):
     def __init__(
         self,
         infile_scf='./scf.xml',
-        infile_dos='./struct_pdos.dat',
+        pdos_glob='./struct_pdos.dat.pdos_atm*',
         outfile_prefix='pdos',
         **kwargs,
     ):
         super().__init__(**kwargs)
         self.infile_scf: str = infile_scf
-        self.infile_dos: str = infile_dos
+        self.pdos_glob: str = pdos_glob
         self.outfile_prefix: str = outfile_prefix
-        
+        self.pdos_files = sorted(glob.glob(self.pdos_glob))
         self.get_data()
         self.set_figures()
 
     def get_data(self):
-        # Get fermi energy. 
+        # Get fermi energy
         tree = ET.parse(self.infile_scf)
         root = tree.getroot()
-        fermi_energy = float(root.findall('.//fermi_energy')[0].text)*Hartree
+        fermi_energy = float(root.findall('.//fermi_energy')[0].text) * Hartree
 
-        data = np.loadtxt(self.infile_dos, skiprows=1)
-        self.axis = (data[:, 0] - fermi_energy).reshape(-1, 1)
-        self.dos = data[:, 1].reshape(-1, 1)
-
-        # Get name.
+        # Get name
         inputdict: dict = self.inputdict
         active_idx: int = jmespath.search('structures.active_idx', inputdict)
         self.struct_name: str = jmespath.search(f'structures.list[{active_idx}].name', inputdict)
 
-        # Build dataframe with x + y's
-        dos_data = pd.DataFrame(
-            np.concat([self.axis, self.dos], axis=1),
-            columns=["x"] + ['y']
-        )
-
-        append_dset_df: pd.DataFrame = pd.DataFrame([
-            {
-                "name": "dset_dos",
-                "data": dos_data  # Store df as a single object in one row
-            },
-        ])
-
-        self.dsets_df = pd.concat([self.dsets_df, append_dset_df], ignore_index=True)
+        for pdos_file in self.pdos_files:
+            # Read file, skip header
+            data = np.loadtxt(pdos_file, skiprows=1)
+            energy = data[:, 0] - fermi_energy
+            ldos = data[:, 1]
+            dos_data = pd.DataFrame({'x': energy, 'y': ldos})
+            dset_name = pdos_label_from_filename(pdos_file)
+            append_dset_df = pd.DataFrame([
+                {
+                    "name": dset_name,
+                    "data": dos_data,
+                },
+            ])
+            self.dsets_df = pd.concat([self.dsets_df, append_dset_df], ignore_index=True)
 
     def set_figures(self):
-        append_fig_df: pd.DataFrame = pd.DataFrame([
-            {
-                'fig_name': self.outfile_prefix,
-                'figure': None, 'subplot_nrow': 1, 'subplot_ncol': 1, 'subplot_idx': 1,
-                'plot_type': PlotType.LINE, 'axis': None,
-                'xlabel': 'Energy (eV)', 'xlim': (-10, 10), 'xticks': None, 'xtick_labels': None,
-                'ylabel': 'DOS', 'ylim': None, 'yticks': None, 'ytick_labels': None,
-                'zlabel': None, 'zlim': None, 'zticks': None, 'ztick_labels': None,
-                'z_inc': None, 'z_azim': None,
-                'title': f'{self.struct_name} DOS',
-                'dset_name': 'dset_dos',
-                'dset_axis_cols': 'x',        
-                'dset_data_cols': ['y'],
-                'color': 'blue', 
-                'xgrid': True,
-                'ygrid': False,
-                'legend_label': None,
-            },
-        ])
+        # Set colors.
+        colors = plt.cm.tab20(np.linspace(0, 1, len(self.pdos_files)))
 
-        self.figs_df = pd.concat([self.figs_df, append_fig_df], ignore_index=True)  
+        # Individual figures
+        for pdos_file, color in zip(self.pdos_files, colors):
+            label = pdos_label_from_filename(pdos_file)
+            
+            append_fig_df = pd.DataFrame([
+                {
+                    'fig_name': f'pdos_{label}',
+                    'figure': None, 'subplot_nrow': 1, 'subplot_ncol': 1, 'subplot_idx': 1,
+                    'plot_type': PlotType.LINE, 'axis': None,
+                    'xlabel': 'Energy (eV)', 'xlim': (-10, 10), 'xticks': None, 'xtick_labels': None,
+                    'ylabel': 'PDOS', 'ylim': None, 'yticks': None, 'ytick_labels': None,
+                    'zlabel': None, 'zlim': None, 'zticks': None, 'ztick_labels': None,
+                    'z_inc': None, 'z_azim': None,
+                    'title': f'{self.struct_name} PDOS ({label})',
+                    'dset_name': label,
+                    'dset_axis_cols': 'x',
+                    'dset_data_cols': ['y'],
+                    'color': 'blue',
+                    'xgrid': True,
+                    'ygrid': False,
+                    'legend_label': None,
+                },
+            ])
+
+            append_overlay_df = pd.DataFrame([
+                {
+                    'fig_name': f'pdos_overlay',
+                    'figure': None, 'subplot_nrow': 1, 'subplot_ncol': 1, 'subplot_idx': 1,
+                    'plot_type': PlotType.LINE, 'axis': None,
+                    'xlabel': 'Energy (eV)', 'xlim': (-10, 10), 'xticks': None, 'xtick_labels': None,
+                    'ylabel': 'PDOS', 'ylim': None, 'yticks': None, 'ytick_labels': None,
+                    'zlabel': None, 'zlim': None, 'zticks': None, 'ztick_labels': None,
+                    'z_inc': None, 'z_azim': None,
+                    'title': f'{self.struct_name} PDOS',
+                    'dset_name': label,
+                    'dset_axis_cols': 'x',
+                    'dset_data_cols': ['y'],
+                    'color': color,
+                    'xgrid': True,
+                    'ygrid': False,
+                    'legend_label': label,
+                },
+            ])
+
+            self.figs_df = pd.concat([self.figs_df, append_fig_df, append_overlay_df], ignore_index=True)
 
 #endregion
